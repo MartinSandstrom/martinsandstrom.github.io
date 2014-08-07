c2fc9dfb20d3b1c3a1b76aeed084316fd2bb3fcb
MartinSandstrom <Martin.sandstrom@avanade.com>
2014-08-07 14:18:33 +0200

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            css: {
+                files: ['**/*.css'],
+                tasks: [],
+                options: {
+                  livereload: '<%= connect.options.livereload %>'
+                }   
+            },
+            html: {
+                files: ['**/*.html'],
+                tasks: [],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '**/*.html',
+                    '**/*.css',
+                ]
+            }
+        },
+        connect: {
+            options:{ }
+                port: 9000,
+                livereload: 35729,
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    open: true
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+
+    grunt.registerTask('default', ['connect:livereload','watch']);
+};
